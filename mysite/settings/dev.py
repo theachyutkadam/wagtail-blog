affb9ce8b89379359be88f843c0d364cf9cf478b
achyut kadam <achyut.k@acldigital.com>
2022-11-30 12:24:40 +0530

    Initial commit of wagtail

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-9+4@x1@l91d#n=_2*35^jj9g(+w#@$hhg585*q&g$e(63@ma1-"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
